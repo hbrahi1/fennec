e9fd9232f769266bd4f3dbe40029a33bc265c362
hbrahi1 <extradisk@yahoo.com>
2015-09-06 22:29:10 -0400

    Initial Load

---
+++ src/main/java/fennec/FennecApplication.java
@@ -0,0 +1,12 @@
+package fennec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FennecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FennecApplication.class, args);
+    }
+}
---
+++ src/main/java/fennec/configuration/ThymeleafConfig.java
@@ -0,0 +1,22 @@
+package fennec.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+/**
+ * Created by Win7 on 06/09/2015.
+ */
+@Configuration
+public class ThymeleafConfig {
+
+//    @Bean
+//    public ServletContextTemplateResolver defaultTemplateResolver() {
+//        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+//        resolver.setPrefix("/templates/");
+//        resolver.setSuffix(".html");
+//        resolver.setTemplateMode("LEGACYHTML5");
+//        resolver.setCacheable(false);
+//        return resolver;
+//    }
+
+}
